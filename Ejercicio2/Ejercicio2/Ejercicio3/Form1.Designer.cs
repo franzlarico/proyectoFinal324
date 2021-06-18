a377b8eb9bd6b648cee959bb9094670c5a3e70e3
franzlarico <73504974+franzlarico@users.noreply.github.com>
2021-06-17 22:42:58 -0400

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Ejercicio3/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RTextura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GTextura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BTextura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rcolor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gcolor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BColor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.nomText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 94);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar textura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Nombre,
+            this.RTextura,
+            this.GTextura,
+            this.BTextura,
+            this.Rcolor,
+            this.Gcolor,
+            this.BColor});
+            this.dataGridView1.Location = new System.Drawing.Point(18, 371);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(942, 150);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Visible = false;
+            this.id.Width = 125;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // RTextura
+            // 
+            this.RTextura.HeaderText = "RTextura";
+            this.RTextura.MinimumWidth = 6;
+            this.RTextura.Name = "RTextura";
+            this.RTextura.Width = 125;
+            // 
+            // GTextura
+            // 
+            this.GTextura.HeaderText = "GTextura";
+            this.GTextura.MinimumWidth = 6;
+            this.GTextura.Name = "GTextura";
+            this.GTextura.Width = 125;
+            // 
+            // BTextura
+            // 
+            this.BTextura.HeaderText = "BTextura";
+            this.BTextura.MinimumWidth = 6;
+            this.BTextura.Name = "BTextura";
+            this.BTextura.Width = 125;
+            // 
+            // Rcolor
+            // 
+            this.Rcolor.HeaderText = "Rcolor";
+            this.Rcolor.MinimumWidth = 6;
+            this.Rcolor.Name = "Rcolor";
+            this.Rcolor.Width = 125;
+            // 
+            // Gcolor
+            // 
+            this.Gcolor.HeaderText = "GColor";
+            this.Gcolor.MinimumWidth = 6;
+            this.Gcolor.Name = "Gcolor";
+            this.Gcolor.Width = 125;
+            // 
+            // BColor
+            // 
+            this.BColor.HeaderText = "BColor";
+            this.BColor.MinimumWidth = 6;
+            this.BColor.Name = "BColor";
+            this.BColor.Width = 125;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 262);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 20);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Borrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // nomText
+            // 
+            this.nomText.Location = new System.Drawing.Point(15, 45);
+            this.nomText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nomText.Name = "nomText";
+            this.nomText.Size = new System.Drawing.Size(95, 20);
+            this.nomText.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Nombre Textura";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 356);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(15, 179);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 21);
+            this.button6.TabIndex = 24;
+            this.button6.Text = " Clasificar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 542);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nomText);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RTextura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GTextura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BTextura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rcolor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gcolor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BColor;
+        private System.Windows.Forms.TextBox nomText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
